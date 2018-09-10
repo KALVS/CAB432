5233a2a228ed8620cc670ceb7a17038e973f763b
KALVS <a.holm57@hotmail.com>
2018-09-10 23:57:23 +1000

    first and second API Working
    Start Google Graph and Report

--- assignment1/app/routes/index.js
+++ assignment1/app/routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
+var request = require('request-promise');
 
 const Recipe = require(__dirname + `/../models/recipe.model`);
 const config = require(__dirname + '/../config.js');
 
 const food = {
-  api_key: 'b7313c432692515177bb2ad599cf3d9d',
+  api_key: 'b41f605b86f73ec4e16a067aba255468',
   nojsoncallback: 1
 }
 
@@ -17,7 +17,7 @@ function cleanString(str, dirtyChar, cleanChar) {
 
 
 
-function searchForRecipe(food, query) {
+async function searchForRecipe(food, query) {
   let recipes = [];
   let foodRawURL = `https://www.food2fork.com/api/search?key=${food.api_key}` +
   `&q=${query}`;
@@ -56,47 +56,52 @@ const edamam = {
 }
 
 async function getRecipe(edamam, recipes) {
+
   var newRecipes = [];
   console.log("The lenght of the recipes: " + recipes.length);
-  return new Promise(resolve => {
-    for (var j = 0; j < recipes.length; j++) {
-      var edamRawURL = `https://api.edamam.com/search?q=${recipes[j].title}&app_id=b3684a31&app_key=94271fc667d95d0240640d96d0527c5c`
-      edamRawURL = cleanString(edamRawURL, /’/g, '');
-      var edamURL = encodeURI(edamRawURL);
-      let rsp;
-      let title = recipes[j].title;
-      request(edamURL, function (error, response, body) {
-        console.log('getRecipe start');
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        rsp = JSON.parse(body);
-        try {
-          let recipe = {
-            title: title,
-            recipeImg: rsp.hits[j].recipe.image,
-            source_URL : rsp.hits[j].recipe.url,
-            ingredients : rsp.hits[j].recipe.ingredients,
-            yield : rsp.hits[j].recipe.yield,
-            energy : rsp.hits[j].recipe.totalDaily.ENERC_KCAL.quantity/rsp.hits[j].recipe.yield,
-            fat : rsp.hits[j].recipe.totalDaily.FAT.quantity/rsp.hits[j].recipe.yield,
-            carbs : rsp.hits[j].recipe.totalDaily.CHOCDF.quantity/rsp.hits[j].recipe.yield,
-            protein : rsp.hits[j].recipe.totalDaily.PROCNT.quantity/rsp.hits[j].recipe.yield,
-          }
-          newRecipes[j] = recipe
-          console.log("try",recipe);
-
-        } catch (e) {
-          console.log("catch failed in getRecipe \n error:" ,e);
+  for (var j = 0; j < recipes.length; j++) {
+    var edamRawURL = `https://api.edamam.com/search?q=${recipes[j].title}&app_id=b3684a31&app_key=94271fc667d95d0240640d96d0527c5c`
+    edamRawURL = cleanString(edamRawURL, /’/g, '');
+    var edamURL = encodeURI(edamRawURL);
+    let rsp;
+    let title = recipes[j].title;
+    await request(edamURL, function (error, response, body) {
+      console.log('getRecipe start');
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      rsp = JSON.parse(body);
+      try {
+        let recipe = {
+          title: title,
+          recipeImg: rsp.hits[j].recipe.image,
+          source_URL : rsp.hits[j].recipe.url,
+          ingredients : rsp.hits[j].recipe.ingredientLines,
+          yield : rsp.hits[j].recipe.yield,
+          energy : rsp.hits[j].recipe.totalDaily.ENERC_KCAL.quantity/rsp.hits[j].recipe.yield,
+          fat : rsp.hits[j].recipe.totalDaily.FAT.quantity/rsp.hits[j].recipe.yield,
+          carbs : rsp.hits[j].recipe.totalDaily.CHOCDF.quantity/rsp.hits[j].recipe.yield,
+          protein : rsp.hits[j].recipe.totalDaily.PROCNT.quantity/rsp.hits[j].recipe.yield,
         }
-      });
-    }
-    resolve(newRecipes);
-  });
+        console.log(recipe.ingredients);
+        newRecipes[j] = recipe
+        //console.log("try",recipe);
+
+      } catch (e) {
+        console.log("catch failed in getRecipe \n error:" ,e);
+      }
+    }); console.log(newRecipes);
+        return(newRecipes);
+
+
+  };
 }
 
 async function doitAll(food, query, edamam) {
-  let first = await searchForRecipe(food, query);//.then(recipes => {
-  let second = await getRecipe(edamam, first);
+  let first = await searchForRecipe(food, query);
+
+  console.log("First", first);//.then(recipes => {
+  let second =  await getRecipe(edamam, first);
+  console.log("second", second);
 
   return second;
 }
@@ -108,8 +113,10 @@ router.get('/', function(req, res, next) {
 
 router.post('/', async function(req, res, next) {
   let query = req.body.Search;
-  const second = await doitAll(food, query, edamam);
-    res.render('index', {title: "You've searched for: " + query, recipes: second});
+  const first = await searchForRecipe(food, query);
+  const second = await getRecipe(edamam, first);
+  console.log("second in post", second);
+  res.render('index', {title: "You've searched for: " + query, recipes: second});
   });
 //});
 
